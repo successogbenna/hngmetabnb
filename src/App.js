0f18bdf58b5f9761913896a55b5d19971c9eebe2
tezrat <successzero7@gmail.com>
2022-11-16 00:23:47 +0100

    context adjusted footer and place done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Section from './components/Section';
+import Place from './components/Place';
 function App() {
   return (
     <div className="App">
-      Jesus is Lord
-      
+       <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Section />} />
+          <Route path='/place' element={<Place />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+function Footer() {
+    return (
+       <div className="footer">
+      <div className="container">
+        <div className="foot">
+          <div>
+            <Link to='/'>
+              <img src="/assets/meta-footer-logo.svg" alt="footer" />
+            </Link>
+            <div className="social">
+              <img src="/assets/facebook.svg" alt="facebook" />
+              <img src="/assets/instagram.svg" alt="instagram" />
+              <img src="/assets/twitter.svg" alt="twitter" />
+            </div>
+            <p>
+              © 2022 Metabnb
+            </p>
+          </div>
+          <div className="sub-foot">
+          <div className='footer-link'>
+            <h5>Community</h5>
+            <ul>
+              <li>NFT</li>
+              <li>Tokens</li>
+              <li>Landlords</li>
+              <li>Discord</li>
+            </ul>
+          </div>
+
+          <div className='footer-link'>
+            <h5>Places</h5>
+            <ul>
+              <li>Castle</li>
+              <li>Farms</li>
+              <li>Beach</li>
+              <li>Learn more</li>
+            </ul>
+          </div>
+
+          <div className='footer-link'>
+            <h5>About us</h5>
+            <ul>
+              <li>Road map</li>
+              <li>Creators</li>
+              <li>Career</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+    );
+}
+
+export default Footer;
